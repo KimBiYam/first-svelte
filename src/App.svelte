05ac9887f1ae4b2a767f55392f07754e047a98db
ChangHyunKim <hot9998@naver.com>
2022-02-02 00:49:43 +0900

    Implement count

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,20 @@
 <script>
-	export let name;
+  let count = 0;
+
+  const increment = () => {
+    count = count + 1;
+  };
+
+  const decrement = () => {
+    count = count - 1;
+  };
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+  <h1>Count : {count}</h1>
+  <button type="button" on:click={increment}>Increment</button>
+  <button type="button" on:click={decrement}>Decrement</button>
 </main>
 
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
 </style>
